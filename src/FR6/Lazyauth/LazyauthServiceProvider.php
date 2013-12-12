c926967e6d1e12419b0320285333bd3465635f06
Francis Houle <fhoule@cloudraker.com>
2013-12-12 15:48:18 -0500

    Initial commit

---
+++ src/FR6/Lazyauth/LazyauthServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace FR6\Lazyauth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LazyauthServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('fr6/lazyauth');
+
+		include __DIR__.'/../../filters.php';
+		include __DIR__.'/../../routes.php';
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
